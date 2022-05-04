7e132500ea43c1970929f6dae9e09b7699ec1bc8
MRX <94920917+MRX-72@users.noreply.github.com>
2022-05-04 21:36:07 +0530

    Create BP.cpp

---
+++ BP.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<unistd.h>
+#include<memory>
+#include<vector>
+#include<utility>
+#include<fstream>
+
+#define xn std::cout<<"\n";
+#define l(x) std::cout<<x<<"\n";
+#define gln(x) std::getline(cin,x)
+
+using namespace std;
+
+int main()
+{
+    l("Code");
+    xn xn return 0;
+}
